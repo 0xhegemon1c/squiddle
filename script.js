0528b33d319d2b56e1c39e44e49111a5626bd268
Squiddy <34935667+0xhegemon1c@users.noreply.github.com>
2024-08-29 02:30:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-function showTalks() {
-    var talksContent = document.getElementById("talksContent");
-    if (talksContent.style.display === "none" || talksContent.style.display === "") {
-        talksContent.style.display = "block";
-    } else {
-        talksContent.style.display = "none";
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-    var pet = new eSheep();
-    pet.Start();
+    if (window.innerWidth > 768) {
+        var pet = new eSheep();
+        pet.Start();
+    }
 });
 
 function showTalks() {
